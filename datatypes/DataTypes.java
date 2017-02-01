b464ed43dc9d4547dfb7d1083801de052e04cc09
Ramesh Khairandish <r.khairandish@dundee.ac.uk>
2017-02-01 13:00:53 +0000

    Datatypes
    The Different Datatypes

---
+++ datatypes/DataTypes.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datatypes;
+/**
+ *
+ * @author Ramesh
+ */
+public class DataTypes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Each Datatype
+        char letter = 'M';
+        String title = "Java in easy steps";
+        int number = 365;
+        float decimal = 98.6f;
+        boolean result = true;
+        
+        System.out.println("Initial is:" + letter);
+        System.out.println("Book is:" + title);
+        System.out.println("Days are:" + number);
+        System.out.println("Temperature is:" + decimal);
+        System.out.println("Answer is:" + result);
+                     
+    }
+    
+}
